16fe21a1fa256d875c9cdb10b2f0b036afcfe42e
samuelsemmler <samuelsemmler@hotmail.com>
2021-04-17 17:25:34 -0300

    Crud initial 'Estoque, Produto, Fornecedor' database created

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.base import Model
+from datetime import date
 
 # Create your models here.
+# class Entidade(models.Model):
+#     entidade_nome = models.CharField(max_length=200)
+#     #True - Supervisor / False - Vendedor
+#     entidade_status = models.BooleanField(default=False)
+
+
+class Estoque(models.Model):
+    estoque_produto_total = models.IntegerField(default=1)
+    estoque_entrada_produto = models.IntegerField(default=10)
+    estoque_saida_produto = models.IntegerField(default=10)
+    produto_pk = models.ForeignKey('Produto', on_delete=models.CASCADE)
+
+
+class Produto(models.Model):
+    produto_nome = models.CharField(max_length=200, default='meu nome')
+
+    produto_quantidade = models.IntegerField(default=1)
+    produto_nome_vendedor = models.CharField(max_length=200, default='Juriscleyton')
+    produto_venda_data = models.DateField(default=date.today())
+    fornecedor_pk = models.ManyToManyField('Fornecedor')
+
+
+class Fornecedor(models.Model):
+    fornecedor_nome = models.CharField(max_length=200, default='Toninho')
