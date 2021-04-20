d0fe9b6b8c7f9971a124aa343869cebecd6277af
samuelsemmler <samuelsemmler@hotmail.com>
2021-04-20 14:00:04 -0300

    crud App deleted

--- crud/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, users
-
-app_name = 'crud'
-urlpatterns = [
-    path('', index, name='index'),
-    path('users/', users, name='users'),
-]
--- supermarket/settings.py
+++ supermarket/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crud.apps.CrudConfig',
     'suppliers.apps.SuppliersConfig'
 ]
 
