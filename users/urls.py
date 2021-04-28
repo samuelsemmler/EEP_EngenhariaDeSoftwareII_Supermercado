8db91316797804a05f6364199c635576325e5a0c
samuelsemmler <samuelsemmler@hotmail.com>
2021-04-28 09:56:42 -0300

    user Authentication completed

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
     path('profile/', profile, name='profile'),
 ]
