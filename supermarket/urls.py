15ec067471a2b2cc62c2e7867a4da6adaacef41c
Antolos <acdnf@yahoo.com.br>
2021-06-12 11:23:39 -0300

    index pagina criada

--- supermarket/urls.py
+++ supermarket/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products.views import products_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products_list ),
     path('products/', include('products.urls')),
     path('suppliers/', include('suppliers.urls')),
     path('users/', include('users.urls')),
